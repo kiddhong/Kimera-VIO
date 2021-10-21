9d0d4ea3c92ee7e085d2e7eaa347cdef0f520e8e
Jehong Lee <ljh@robotis.com>
2021-10-21 10:39:16 -0400

    Revised some configurations
    Signed-off-by: Jehong Lee <ljh@robotis.com>

--- scripts/docker/kimera_vio_docker.bash
+++ scripts/docker/kimera_vio_docker.bash
@@ -2,7 +2,7 @@
 
 # Allow X server connection
 xhost +local:root
-docker run -it --rm \
+docker run --gpus all -it --rm \
     --env="DISPLAY" \
     --env="QT_X11_NO_MITSHM=1" \
     --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
